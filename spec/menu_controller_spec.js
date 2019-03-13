d5a5d39efef626dddacf4f15a03c34b041ac11d3
lawrence <lawrence.t.yen@gmail.com>
2019-03-13 15:02:22 -0700

    connect-db-assignment

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,19 +6,6 @@ describe('MenuController', () => {
     this.menu = new MenuController();
   })
 
-  describe('#getContactCount()', () => {
-
-    it('Should return 0 when no contacts are in the book', () => {
-      expect(this.menu.getContactCount()).toBe(0);
-    });
-
-    it('Should return 1 when one contact is in the book', () => {
-      this.menu.contacts.push('Bob');
-      expect(this.menu.getContactCount()).toBe(1);
-    })
-
-  });
-
   describe('#reminder()', () => {
 
     it('Should remind you about learning',() => {
